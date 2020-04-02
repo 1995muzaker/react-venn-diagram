f75eecb37bcf8b4416eee65565776978f942ecd3
Shaikh Muzaker <shaikhmuzaker@gmail.com>
2020-04-02 15:03:40 +0530

    venn diagram integrated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
+import * as d3 from "d3";
+import * as venn from "venn.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var sets = [
+  {
+    size: 3411,
+    sets: ["91", "101", "111"]
+  },
+  {
+    size: 5039,
+    sets: ["91", "101"]
+  },
+  {
+    size: 4829,
+    sets: ["91", "111"]
+  },
+  {
+    size: 14861,
+    sets: ["101", "111"]
+  },
+  {
+    size: 21792,
+    sets: ["91"]
+  },
+  {
+    size: 35261,
+    sets: ["101"]
+  },
+  {
+    size: 37272,
+    sets: ["111"]
+  }
+];
+
+class App extends Component {
+  chart = venn.VennDiagram();
+  componentDidMount() {
+    let div = d3.select(this.refs.chart);
+    div.datum(sets).call(this.chart);
+  }
+  render() {
+    return (
+      <div className="" ref="chart">
+      </div>
+    );
+  }
 }
 
 export default App;
